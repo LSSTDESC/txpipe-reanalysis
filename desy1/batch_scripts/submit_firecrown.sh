eebd078ed43291e1b7c69c4559f7ea6cdefeaaaf
ellongley <elp25@duke.edu>
2020-02-26 10:35:47 -0800

    added submit_firecrown to include running the MCMC.

--- desy1/batch_scripts/submit_firecrown.sh
+++ desy1/batch_scripts/submit_firecrown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash   
 #SBATCH -N 2
-#SBATCH --qos=premium
-#SBATCH --time=0:20:00
+#SBATCH --qos=regular
+#SBATCH --time=15:00:00
 #SBATCH --job-name=FireCrown_Test
 #SBATCH --image=docker:joezuntz/txpipe_cosmosis_firecrown:latest
 #SBATCH --license=SCRATCH
@@ -13,4 +13,6 @@ export OMP_NUM_THREADS=1
 source setup-firecrown
 
 
-srun -n  64 shifter --volume=$PWD/cosmosis:/opt/cosmosis --volume $PWD/FireCrown/:/opt/firecrown --volume $PWD/TXPipe:/opt/txpipe firecrown compute /global/cscratch1/sd/elp25/FireCrown/examples/cosmicshear/cosmicshear.yaml 
+srun -n  64 shifter --volume=/global/cscratch1/sd/elp25/cosmosis:/opt/cosmosis --volume /global/cscratch1/sd/elp25/FireCrown/:/opt/firecrown --volume /global/cscratch1/sd/elp25/TXPipe:/opt/txpipe firecrown compute /global/cscratch1/sd/elp25/FireCrown/examples/des_y1_3x2pt/des_y1_3x2pt.yaml 
+
+srun -n  64 shifter --volume=/global/cscratch1/sd/elp25/cosmosis:/opt/cosmosis --volume /global/cscratch1/sd/elp25/FireCrown/:/opt/firecrown --volume /global/cscratch1/sd/elp25/TXPipe:/opt/txpipe firecrown run-emcee /global/cscratch1/sd/elp25/FireCrown/examples/des_y1_3x2pt/des_y1_3x2pt.yaml
